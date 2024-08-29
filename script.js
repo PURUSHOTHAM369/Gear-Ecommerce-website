86c5ae77f687a2d7d0905e1ffa2ae7dc087ce5aa
simplilearn_Github <purushothamyadav569@simplilearn.net>
2024-08-29 06:02:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function redirectToLogin() {
+    window.location.href = "login.html";
+  }
+
+function redirectToOrder(){
+  window.location.href = "orderplaced.html";
+}
+function redirectToContact(){
+  window.location.href = "send.html";
+}
+
